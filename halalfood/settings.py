3a00f03f51df71e0fe94df964a673415329cc0c6
Misha <58331372+chry786@users.noreply.github.com>
2022-01-29 22:49:19 +0000

    Created header and footer

--- halalfood/settings.py
+++ halalfood/settings.py
@@ -126,5 +126,7 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+MEDIA_URL = '/images/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
